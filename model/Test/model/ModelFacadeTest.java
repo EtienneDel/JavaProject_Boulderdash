81567f5f6c5be59a4421c143987c08fce20906f7
EtienneDel <etienne.delnott@viacesi.fr>
2017-06-14 16:20:40 +0200

    Creating test classes

---
+++ model/Test/model/ModelFacadeTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelFacadeTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getExampleById() throws Exception {
+    }
+
+    @Test
+    public void getExampleByName() throws Exception {
+    }
+
+    @Test
+    public void getAllExamples() throws Exception {
+    }
+
+}
