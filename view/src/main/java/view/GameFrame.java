2cbc003298b916fb3422334a758813af2a4ede62
phousphoure <paul.brouet@viacesi.fr>
2017-06-15 09:36:36 +0200

    creation of GameFrame class(package view)

---
+++ view/src/main/java/view/GameFrame.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by ASUS on 15/06/2017.
+ */
+public class GameFrame extends JFrame {
+
+
+private GamePanel panel;
+
+
+    public GameFrame(){
+        super();
+        panel = new GamePanel();
+    }
+
+}
