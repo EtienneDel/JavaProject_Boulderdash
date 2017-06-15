8626d7ee92b1c3cbd9299c17c039789bfd2644fb
phousphoure <paul.brouet@viacesi.fr>
2017-06-15 13:02:09 +0200

    Updates class position
    getter and setter

--- model/src/main/java/model/Position.java
+++ model/src/main/java/model/Position.java
@@ -11,4 +11,20 @@ public class Position {
         this.posY = y;
 
     }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
