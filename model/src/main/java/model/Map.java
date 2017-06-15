296359ef675881942d77cf670cd9e9b7cd9cd042
phousphoure <paul.brouet@viacesi.fr>
2017-06-15 11:07:17 +0200

    Update of Map class

--- model/src/main/java/model/Map.java
+++ model/src/main/java/model/Map.java
@@ -2,10 +2,13 @@ package model;
 
 import model.dao.MapDAO;
 
+import java.util.ArrayList;
+
 public class Map {
 
     private Tile tile;
     private MapDAO mapDAO;
+    private ArrayList<Tile> tiles;
 
     public Map(Tile tile, MapDAO mapDAO){
         this.tile = tile;
