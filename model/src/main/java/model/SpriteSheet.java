f241024adae910c9518e03d3206f4b323c9f71f7
EtienneDel <etienne.delnott@viacesi.fr>
2017-06-16 09:10:02 +0200

    Model update and Spritesheet

---
+++ model/src/main/java/model/SpriteSheet.java
@@ -0,0 +1,14 @@
+package model;
+
+
+import javax.swing.*;
+
+public class SpriteSheet extends JLabel{
+
+    public SpriteSheet() {
+        ImageIcon img = new ImageIcon("C:\\Users\\Etienne\\Desktop\\JavaProject\\JavaProject_Boulderdash\\SpriteSheet.png");
+
+    }
+
+
+}
