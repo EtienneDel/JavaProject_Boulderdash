7979d9df4c92a42ed5927e11ffd32f9258554db6
Itega <julien.zolli1@gmail.com>
2017-06-17 16:02:52 +0200

    controller: edit rock controller
    refresh function + polymorph + setTimeout

--- controller/src/main/java/controller/RockController.java
+++ controller/src/main/java/controller/RockController.java
@@ -1,29 +1,80 @@
 package controller;
 
+import model.Position;
 import model.Rock;
+import model.Tile;
+
+import java.util.ArrayList;
 
 /**
  * Created by Julien on 16/06/2017.
  */
-public class RockController extends Rock{
-    public RockController(int x, int y) {
-        super(x, y);
+public class RockController{
+
+    private ArrayList<Rock> rockList;
+
+    public RockController(ArrayList<Rock> rockList) {
+        this.rockList = rockList;
     }
 
     public void refresh() {
-        int bottomTile = getTileByPos(position.getPosX(), position.getPosY() - 1);
-
-        while(bottomTile == '0' || bottomTile == '7' || bottomTile == '2') {
-            if(bottomTile == '7')
-                explode(true, position.getPosX(), position.getPosY() - 1);
-            else if(bottomTile == '0')
-                explode(false, position.getPosX(), position.getPosY() - 1);
-            else
-                setPosition(position.getPosX(), position.getPosY() - 1);
+        for(Rock rock: rockList) {
+            refresh(rock);
         }
     }
 
-    private void explode(boolean diamondShower, int x, int y) {
+    public void refresh(Position position) {
+        Rock rock = new Rock(0,0);
+        refresh(rock.getTileByPos(position.getPosX(), position.getPosY()));
+    }
+
+    public void refresh(Rock rock){
+        Position position = rock.getPosition();
+
+        Tile bottomTile = rock.getTileByPos(position.getPosX(), position.getPosY()-1);
+        Tile leftTile = rock.getTileByPos(position.getPosX()-1, position.getPosY());
+        Tile rightTile = rock.getTileByPos(position.getPosX()+1, position.getPosY());
+        Tile bottomLeftTile = rock.getTileByPos(position.getPosX()-1, position.getPosY());
+        Tile bottomRightTile = rock.getTileByPos(position.getPosX()+1, position.getPosY());
+
+        int ref = bottomTile.getRef();
+        if (ref == '7')//monstre
+            explode(true, position);
+        else if (ref == '0')//bloc cassable
+            explode(false, position);
+        else if (ref == '2')//vide
+            rock.moveD(position);
+        else {
+            ref = bottomLeftTile.getRef();
+            if (leftTile.getRef() == '2' && (ref == '0' || ref == '2' || ref == '7'))//vide
+                rock.moveL(position);
+            else {
+                ref = bottomRightTile.getRef();
+                if (rightTile.getRef() == '2' && (ref == '0' || ref == '2' || ref == '7'))//vide
+                    rock.moveR(position);
+            }
+        }
+
+        position = rock.getPosition();
+        bottomTile = rock.getTileByPos(position.getPosX(), position.getPosY()-1);
+
+        if(bottomTile.getRef() == '2')
+            setTimeout(() -> refresh(rock), 500);
+    }
+
+    private void explode(boolean diamondShower, Position position) {
         
+    }
+
+    public void setTimeout(Runnable runnable, int delay){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
     }
 }
