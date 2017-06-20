dc497839caccbec8b0919699673fdf16cf4421b2
yanismercier <yanis.mercier@viacesi.fr>
2017-06-20 16:12:33 +0200

    buid fix

--- controller/src/main/java/controller/Build.java
+++ controller/src/main/java/controller/Build.java
@@ -5,6 +5,9 @@ package controller;
  */
 import model.*;
 import view.IView;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Build implements IController {
@@ -16,47 +19,54 @@ public class Build implements IController {
         this.view = view;
         this.model = model;
         Assets assets = new Assets();
+        BufferedImage tmp = new BufferedImage(10000,10000, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = tmp.createGraphics();
+        
+        for(int a=0;a<10;a++){
 
-        for (int y = 0; y < model.getMap_height(); y++) {//TODO Déplacer dans model
+        for (int y = 0; y < model.getMap_height(); y++) {
             for (int x = 0; x < model.getMap_width(); x++) {
                 int block = (int) model.getTab_map()[x][y] - 48;
 
                 switch (block) {
 
-                    case Dirt.REF:
-                        view.drawImage(assets.dirt, x * 16, y * 16);
-                        break;
+                case Dirt.REF:
+                	g.drawImage(assets.dirt, (x+a)*16, y*16, null);
+                    break;
 
-                    case Wall.REF:
-                        view.drawImage(assets.wall, x * 16, y * 16);
-                        break;
+                case Wall.REF:
+                	g.drawImage(assets.wall, (x+a)*16, y*16, null);
+                    break;
 
-                    case Empty.REF:
-                        view.drawImage(assets.empty, x * 16, y * 16);
-                        break;
+                case Empty.REF:
+                	g.drawImage(assets.empty, (x+a)*16, y*16, null);
+                    break;
 
-                    case BWall.REF:
-                        view.drawImage(assets.bwall, x * 16, y * 16);
-                        break;
+                case BWall.REF:
+                	g.drawImage(assets.bwall, (x+a)*16, y*16, null);
+                    break;
 
-                    case Diamond.REF:
-                        view.drawImage(assets.diamond, x * 16, y * 16);
-                        break;
+                case Diamond.REF:
+                	g.drawImage(assets.diamond, (x+a)*16, y*16, null);
+                    break;
 
-                    case Rock.REF:
-                        view.drawImage(assets.rock, x * 16, y * 16);
-                        break;
+                case Rock.REF:
+                	g.drawImage(assets.rock, (x+a)*16, y*16, null);
+                    break;
 
-                    case Exit.REF:
-                        view.drawImage(assets.exit, x * 16, y * 16);
-                        break;
-                }
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                case Exit.REF:
+                	g.drawImage(assets.exit, (x+a)*16, y*16, null);
+                    break;
                 }
             }
         }
+        view.drawImage(tmp, 0, 0);
+        try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
     }
 }
