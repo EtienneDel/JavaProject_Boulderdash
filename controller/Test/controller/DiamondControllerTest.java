3b902732a85e8a592926ca7923a7c5ed1bc4cc30
phousphoure <paul.brouet@viacesi.fr>
2017-06-20 11:03:12 +0200

    DiamondControllerTest: creation

---
+++ controller/Test/controller/DiamondControllerTest.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by ASUS on 20/06/2017.
+ */
+public class DiamondControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    @Test
+    public void refresh(){
+
+    }
+}
