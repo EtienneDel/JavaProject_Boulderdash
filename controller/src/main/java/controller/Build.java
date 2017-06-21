4bacd9f1bee6d0c6c513b1a9cbc881dee44be05b
EtienneDel <etienne.delnott@viacesi.fr>
2017-06-21 11:28:56 +0200

    Controller : Moving the map

--- controller/src/main/java/controller/Build.java
+++ controller/src/main/java/controller/Build.java
@@ -3,27 +3,32 @@ package controller;
 /**
  * Created by Etienne on 20/06/2017.
  */
-import model.*;
+
+import model.Assetable;
+import model.IModel;
 import view.IView;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class Build implements IController {
+public class Build {
 
     private final IView view;
     private final IModel model;
+    private BufferedImage map;
+    private int xM = 0, yM = 0;
 
 
     public Build(final IView view, final IModel model) throws IOException {
         this.view = view;
         this.model = model;
+    }
+
+    public void calculateMap(){
         Assetable asset = model.getAssets();
-        BufferedImage tmp = new BufferedImage(10000,10000, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = tmp.createGraphics();
-        
-        for(int a=0;a<10;a++){
+        map = new BufferedImage(10000, 10000, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = map.createGraphics();
 
         for (int y = 0; y < model.getMap_height(); y++) {
             for (int x = 0; x < model.getMap_width(); x++) {
@@ -31,43 +36,49 @@ public class Build implements IController {
 
                 switch (block) {
 
-                case 0:
-                	g.drawImage(asset.getDirt(), (x+a)*16, y*16, null);
-                    break;
+                    case 0:
+                        g.drawImage(asset.getDirt(), x * 16, y * 16, null);
+                        break;
 
-                case 1:
-                	g.drawImage(asset.getWall(), (x+a)*16, y*16, null);
-                    break;
+                    case 1:
+                        g.drawImage(asset.getWall(), x * 16, y * 16, null);
+                        break;
 
-                case 2:
-                	g.drawImage(asset.getEmpty(), (x+a)*16, y*16, null);
-                    break;
+                    case 2:
+                        g.drawImage(asset.getEmpty(), x * 16, y * 16, null);
+                        break;
 
-                case 3:
-                	g.drawImage(asset.getBwall(), (x+a)*16, y*16, null);
-                    break;
+                    case 3:
+                        g.drawImage(asset.getBwall(), x * 16, y * 16, null);
+                        break;
 
-                case 4:
-                	g.drawImage(asset.getDiamonds(), (x+a)*16, y*16, null);
-                    break;
+                    case 4:
+                        g.drawImage(asset.getDiamonds(), x * 16, y * 16, null);
+                        break;
 
-                case 5:
-                	g.drawImage(asset.getRock(), (x+a)*16, y*16, null);
-                    break;
+                    case 5:
+                        g.drawImage(asset.getRock(), x * 16, y * 16, null);
+                        break;
 
-                case 6:
-                	g.drawImage(asset.getExit(), (x+a)*16, y*16, null);
-                    break;
+                    case 6:
+                        g.drawImage(asset.getExit(), x * 16, y * 16, null);
+                        break;
                 }
             }
         }
-        view.drawImage(tmp, 0, 0);
-        try {
-			Thread.sleep(50);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        view.drawImage(map,xM,yM);
     }
+
+    public void setPos(int x, int y){
+        this.xM = x;
+        this.yM = y;
     }
+
+    public int getPosX(){
+        return xM;
+    }
+    public int getPosY(){
+        return yM;
+    }
+
 }
