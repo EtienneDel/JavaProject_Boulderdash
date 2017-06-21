7cdd9368d8933c1d54fe00395c975ba987d6935b
Itega <julien.zolli1@gmail.com>
2017-06-21 20:14:07 +0200

    controller: refresh diamond

--- controller/src/main/java/controller/DiamondController.java
+++ controller/src/main/java/controller/DiamondController.java
@@ -20,12 +20,11 @@ public class DiamondController {
     }
 
     public void refresh(IPosition position) throws IOException {
+        if(!(position.getPosX() > 0 && position.getPosY() > 0 && map.getCharByPos(position.getPosX(), position.getPosY()) == '4'))
+            return;
+
         IDiamond diamond = model.getDiamond();// = map.getRockByPos(position);
         char bottomTile = map.getCharByPos(position.getPosX(), position.getPosY()+1);
-        char leftTile = map.getCharByPos(position.getPosX()-1, position.getPosY());
-        char rightTile = map.getCharByPos(position.getPosX()+1, position.getPosY());
-        char bottomLeftTile = map.getCharByPos(position.getPosX()-1, position.getPosY()-1);
-        char bottomRightTile = map.getCharByPos(position.getPosX()+1, position.getPosY()-1);
 
         if (bottomTile == '7')//monstre
             explode(true, position);
@@ -33,50 +32,24 @@ public class DiamondController {
             explode(false, position);
         else if (bottomTile == '2')//vide
             moveDown(diamond, position);
-        else {
-            if (leftTile == '2' && (bottomLeftTile == '0' || bottomLeftTile == '2' || bottomLeftTile == '7'))//vide
-                diamond.moveL(position);
-            else {
-                if (rightTile == '2' && (bottomRightTile == '0' || bottomRightTile == '2' || bottomRightTile == '7'))//vide
-                    diamond.moveR(position);
-            }
-        }
 
-        position = diamond.getPosition();
-        bottomTile = map.getCharByPos(position.getPosX(), position.getPosY()-1);
-        //permet de ré update le caillou si y'a du vide dessous
-        //todo supprimer ?
-        if(bottomTile == '2') {
-            IPosition finalPosition = position;
-            setTimeout(() -> {
-                try {
-                    refresh(finalPosition);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }, 500);
-        }
+}
+
+    private void moveDown(IDiamond diamond, IPosition position) throws IOException {
+        diamond.moveD(position);
+
+        refreshAround(position);
     }
 
-    private void moveDown(IDiamond diamond, IPosition position) {
-        diamond.setPosition(position);
-        diamond.moveD(position);
+    private void refreshAround(IPosition position) throws IOException {
+        refresh(model.getPosition(position.getPosX(), position.getPosY()-1));
+        refresh(model.getPosition(position.getPosX()-1, position.getPosY()));
+        refresh(model.getPosition(position.getPosX()+1, position.getPosY()));
+        refresh(model.getPosition(position.getPosX(), position.getPosY()+1));
     }
 
     private void explode(boolean diamondShower, IPosition position) {
 
-    }
-
-    public void setTimeout(Runnable runnable, int delay){
-        new Thread(() -> {
-            try {
-                Thread.sleep(delay);
-                runnable.run();
-            }
-            catch (Exception e){
-                System.err.println(e);
-            }
-        }).start();
     }
 }
 
--- controller/src/main/java/controller/MainController.java
+++ controller/src/main/java/controller/MainController.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 /**
  * Created by Julien on 21/06/2017.
  */
-public class MainController implements IPosition, IRock, IMap, IController{
+public class MainController {
     private RockController rockController;
     private DiamondController diamondController;
     private IModel model;
@@ -23,94 +23,10 @@ public class MainController implements IPosition, IRock, IMap, IController{
         rockController = new RockController(model);
         diamondController = new DiamondController(model);
 
-        position.setPosition(3,1);
-        rockController.refresh(position);
-        position.setPosition(4,1);
+        position.setPosition(7,4);
         diamondController.refresh(position);
-        position.setPosition(5,1);
-        rockController.refresh(position);
-        System.out.println(map.getCharByPos(3,1));
-
-        System.out.println(map.getCharByPos(3,1));
+        //position.setPosition(7,3);
+        //diamondController.refresh(position);
     }
 
-    @Override
-    public IPosition getPosition(int x, int y) {
-        return null;
-    }
-
-    @Override
-    public void setPosition(int x, int y) {
-
-    }
-
-    @Override
-    public int getRef() {
-        return 0;
-    }
-
-    @Override
-    public IPosition getPosition() {
-        return null;
-    }
-
-    @Override
-    public void setPosition(IPosition position) {
-
-    }
-
-    @Override
-    public int getPosX() {
-        return 0;
-    }
-
-    @Override
-    public int getPosY() {
-        return 0;
-    }
-
-    @Override
-    public void moveD(IPosition position) {
-
-    }
-
-    @Override
-    public void moveL(IPosition position) {
-
-    }
-
-    @Override
-    public void moveR(IPosition position) {
-
-    }
-
-    @Override
-    public void moveU(IPosition position) {
-
-    }
-
-    @Override
-    public Tilable getRock() {
-        return null;
-    }
-
-    @Override
-    public IMap getTheMap() {
-        return null;
-    }
-
-    @Override
-    public char getCharByPos(int x, int y) {
-        return 'a';
-    }
-
-    @Override
-    public void setCharByPos(int x, int y, char chare) {
-
-    }
-
-    @Override
-    public Tilable getTileByPos(IPosition position) {
-        return null;
-    }
 }
