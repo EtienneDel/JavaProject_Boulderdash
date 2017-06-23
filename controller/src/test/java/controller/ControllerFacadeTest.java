ff05843279c9a1d2e90dec24dab74820da56e84c
phousphoure <paul.brouet@viacesi.fr>
2017-06-23 11:10:02 +0200

    New test in view and controller

--- controller/src/test/java/controller/ControllerFacadeTest.java
+++ controller/src/test/java/controller/ControllerFacadeTest.java
@@ -1,12 +1,19 @@
 package controller;
 
+import model.IModel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import view.IView;
 
 public class ControllerFacadeTest {
+    ControllerFacade test;
+    IModel model;
+
     @Before
     public void setUp() throws Exception {
+
+        test = new ControllerFacade(model);
     }
 
     @After
@@ -19,6 +26,7 @@ public class ControllerFacadeTest {
 
     @Test
     public void getView() throws Exception {
+
     }
 
     @Test
--- view/src/test/java/ViewFacadeTest.java
+++ view/src/test/java/ViewFacadeTest.java
@@ -13,6 +13,7 @@ public class ViewFacadeTest {
 
     @Test
     public void displayMessage() throws Exception {
+        String
     }
 
 }
