494e99e090ea19a15dc4d7f2eef59a67712bfa34
Itega <julien.zolli1@gmail.com>
2017-06-23 08:25:21 +0200

    main: generate javadoc

--- main/src/Test/main/MainTest.java
+++ main/src/Test/main/MainTest.java
@@ -6,15 +6,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * The type Main test.
+ */
 public class MainTest {
+    /**
+     * Sets up.
+     *
+     * @throws Exception the exception
+     */
     @Before
     public void setUp() throws Exception {
     }
 
+    /**
+     * Tear down.
+     *
+     * @throws Exception the exception
+     */
     @After
     public void tearDown() throws Exception {
     }
 
+    /**
+     * Main.
+     *
+     * @throws Exception the exception
+     */
     @Test
     public void main() throws Exception {
     }
